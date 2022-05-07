f9bf2e880a89a80141a1a9201bd54adbac7524e3
Boobeh123 <boobeh321@gmail.com>
2022-05-06 14:47:10 -1000

    AddEventListener to select tag

--- main.js
+++ main.js
@@ -7,16 +7,20 @@ const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
     // Then store JavaScript object into variable (data)
         // Iterate through object properties
             // Render each property as an option element
+            // Append option tags to drop menu
+// Drop menu listens for a click or change in select
+    // Pull value from event.target object and store in variable (url)
+        // Select option values create dynamic url using a template literal 
 
 fetch(BREEDS_URL)
-    .then(response => {                                 // Returns response
-        return response.json();                         // Returns promise with object. Object has message and status properties
+    .then(response => {                                             // Returns response
+        return response.json();                                     // Returns promise with object. Object has message and status properties
     })
     .then(data =>{
-        console.log(data)                               // Returns object. Object has message and status property
-        console.log(data.message)                       // Returns object of arrays
-        console.log(Object.keys(data))                  // Returns object properties in an array -> output -> message, status
-        console.log(Object.keys(data.message))          // Returns object properties in an array -> output -> 95 element array
+        console.log(data)                                           // Returns object. Object has message and status property
+        console.log(data.message)                                   // Returns object of arrays
+        console.log(Object.keys(data))                              // Returns object properties in an array -> output -> message, status
+        console.log(Object.keys(data.message))                      // Returns object properties in an array -> output -> 95 element array
         
         const dataObject= data.message                              // Store object in variable (dataObject)
         const dataObjectProperties = Object.keys(dataObject);       // Store array of object properties in variable (dataObjectProperties)
@@ -28,3 +32,11 @@ fetch(BREEDS_URL)
             document.querySelector('select').appendChild(optionTags);// Append options to select
         }
     })
+
+document.querySelector('select').addEventListener('change', event => {
+    console.log(event);                                 // Returns Event object
+    console.log(event.target);                          // Returns 'select' tag with values
+    console.log(event.target.value);                    // Returns event.target value
+    
+    let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`
+})
